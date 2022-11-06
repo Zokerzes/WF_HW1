572b645c703ab407f6fab1f80614ee2e1aebb7ae
Bliznyuk Roman <romzes85@mail.ru>
2022-11-06 19:11:15 +0300

    Добавьте файлы проекта.

---
+++ WF_HW1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_HW1
+{
+    public partial class meeting : Form
+    {
+        public meeting()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task1 task1 = new Task1();
+            task1.ShowDialog();
+        }
+    }
+}
---
+++ WF_HW1/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_HW1
+{
+    public partial class Task1 : Form
+    {
+        string fio = "Меня зовут Роман";
+        string str = "Я являюсь студентом \"Аккадемии \"Шаг\"\"";
+        string str1 = "И изучаю С#";
+        public Task1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(fio);
+            MessageBox.Show(str);
+            MessageBox.Show(str1);
+        }
+    }
+}
