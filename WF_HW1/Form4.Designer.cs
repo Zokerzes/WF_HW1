17a8ab0c63751a4fa1f52b78aebb2641eca67940
Roman <yozhik13@gmail.com>
2022-11-06 23:58:59 +0300

    ++

--- WF_HW1/Form4.Designer.cs
+++ WF_HW1/Form4.Designer.cs
@@ -28,13 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(50, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(183, 120);
+            this.panel1.TabIndex = 0;
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(272, 173);
+            this.ClientSize = new System.Drawing.Size(284, 211);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 250);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 250);
             this.Name = "Form4";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form4";
@@ -43,5 +58,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- WF_HW1/Form4.cs
+++ WF_HW1/Form4.cs
@@ -15,6 +15,32 @@ namespace WF_HW1
         public Form4()
         {
             InitializeComponent();
+      
+            this.MouseClick += new MouseEventHandler(MouseCl);
+            this.MouseMove += new MouseEventHandler(MouseMove_);
         }
+        private void MouseMove_(object sender, MouseEventArgs m)
+        {
+          
+            this.Text =$" {this.Size.Width}x{this.Size.Height}{m.Location.ToString()}  ";
+        }
+    
+        private void MouseCl(object sender, MouseEventArgs m1)
+        {
+            int x = m1.X;
+            int y = m1.Y;
+            if ((x == 50 || x == this.Width - 67) || (y == 50 || y == this.Height - 80))
+                {
+                    MessageBox.Show("Точка НА ГРАНИЦЕ прямоугольника");
+                }
+            else if ((x < 50 || x > this.Width - 67)||(y < 50 || y > this.Height - 80))
+            {
+                MessageBox.Show("Точка ВНЕ прямоугольника");
+            }
+            else if ((x > 50 || x < this.Width - 67) || (y > 50 || y < this.Height - 80))
+            {
+                MessageBox.Show("Точка ВНУТРИ прямоугольника");
+            }
+        }        
     }
 }
