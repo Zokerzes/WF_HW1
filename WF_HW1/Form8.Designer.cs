65180ba99ec6c24fe3ae37b68e1d0e73e0bcfbc9
Roman <yozhik13@gmail.com>
2022-11-15 21:27:07 +0300

    add task7

--- WF_HW1/Form8.Designer.cs
+++ WF_HW1/Form8.Designer.cs
@@ -63,7 +63,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Ok";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // inYear
             // 
--- WF_HW1/Form8.cs
+++ WF_HW1/Form8.cs
@@ -22,56 +22,65 @@ namespace WF_HW1
         private void dateIn()
         {
             DateTime dateBegin, dateEnd, date;
-            //try
-            //{
+
             dateEnd = Convert.ToDateTime(textBox1.Text);
             dateBegin = DateTime.Now;
 
 
-            textBox2.Text = Convert.ToDateTime(dateEnd.Subtract(dateBegin)).ToString();
-            //date.AddMonths(dateEnd.Month - dateBegin.Month);
-            //date.AddDays(dateEnd.Day - dateBegin.Day);
-            //date.AddHours(dateEnd.Hour - dateBegin.Hour);
-            //date.AddMinutes(dateEnd.Minute - dateBegin.Minute);
-            //date.AddSeconds(dateEnd.Second - dateBegin.Second);
-            //textBox2.Text =date.ToString();
 
-            //textBox2.Text = CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(date.DayOfWeek).ToString();
-            //if (inYear.Checked)
-            //{
-            //    textBox2.Text = (date.Year).ToString();
-            //}
-            //if (inMonth.Checked)
-            //{
-            //    textBox2.Text = (date.Month).ToString();
-            //}
-            //if (inHours.Checked)
-            //{
-            //    textBox2.Text = (date.Hour).ToString();
-            //}
-            //if (inMinute.Checked)
-            //{
-            //    textBox2.Text = (date.Minute).ToString();
-            //}
-            //if (inSecunde.Checked)
-            //{
-            //    textBox2.Text = (date.Second).ToString();
-            //}
-            //}
 
-            //catch
-            //{
-            //    MessageBox.Show("ВНИМАНИЕ: введите дату в цифровом формате дд.мм.гггг");
-            //}
+            TimeSpan ts = dateEnd - dateBegin;
 
+            if (inYear.Checked)
+            {
+                textBox2.Text = (ts.TotalDays / 360).ToString();
+            }
+            if (inMonth.Checked)
+            {
+                textBox2.Text = (ts.TotalDays / 30).ToString();
+            }
+            if (inHours.Checked)
+            {
+                textBox2.Text = (ts.TotalHours).ToString();
+            }
+            if (inMinute.Checked)
+            {
+                textBox2.Text = (ts.TotalMinutes).ToString();
+            }
+            if (inSecunde.Checked)
+            {
+                textBox2.Text = (ts.TotalSeconds).ToString();
+
+                // Define two dates.
+                DateTime date1 = new DateTime(2010, 1, 1, 8, 0, 15);
+                DateTime date2 = new DateTime(2010, 8, 18, 13, 30, 30);
+
+                // Calculate the interval between the two dates.
+
+
+                // Display individual properties of the resulting TimeSpan object.
+                //Console.WriteLine("   {0,-35} {1,20}", "Value of Days Component:", interval.Days);
+                //Console.WriteLine("   {0,-35} {1,20}", "Total Number of Days:", interval.TotalDays);
+                //Console.WriteLine("   {0,-35} {1,20}", "Value of Hours Component:", interval.Hours);
+                //Console.WriteLine("   {0,-35} {1,20}", "Total Number of Hours:", interval.TotalHours);
+                //Console.WriteLine("   {0,-35} {1,20}", "Value of Minutes Component:", interval.Minutes);
+                //Console.WriteLine("   {0,-35} {1,20}", "Total Number of Minutes:", interval.TotalMinutes);
+                //Console.WriteLine("   {0,-35} {1,20:N0}", "Value of Seconds Component:", interval.Seconds);
+                //Console.WriteLine("   {0,-35} {1,20:N0}", "Total Number of Seconds:", interval.TotalSeconds);
+                //Console.WriteLine("   {0,-35} {1,20:N0}", "Value of Milliseconds Component:", interval.Milliseconds);
+                //Console.WriteLine("   {0,-35} {1,20:N0}", "Total Number of Milliseconds:", interval.TotalMilliseconds);
+                //Console.WriteLine("   {0,-35} {1,20:N0}", "Ticks:", interval.Ticks);
+            }
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dateIn();
         }
 
        
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-              dateIn();
-        }
     }
 }
 
